630c362375c2cae627199c7036a26cc5e55cee03
aholm <holmslice86@gmail.com>
2014-09-14 18:01:42 -0700

    Part one of the sticking nav bar quest

--- StormySpaces/PresentationLayer/StormySpaces.Web/Scripts/App/Controllers/home-controller.js
+++ StormySpaces/PresentationLayer/StormySpaces.Web/Scripts/App/Controllers/home-controller.js
@@ -2,9 +2,9 @@
 
     "use strict";
 
-    stormySpaces.controller("homeController", [homeController]);
+    stormySpaces.controller("homeController", ["$window", homeController]);
 
-    function homeController() {
+    function homeController($window) {
 
         var vm = this;
         var _answer = "Yes";
@@ -24,6 +24,27 @@
             panelBody: "We treat all of our customers as if they were family"
         }
         ];
+
+
+        var floatingNav = document.getElementById("floatingNav");
+        var navDistanceFromTop = 500;
+        var isNavFixed = false;
+        angular.element($window).bind("scroll", function () {
+
+            var windowTop = $window.pageYOffset;
+            var shouldBeFixed = windowTop > navDistanceFromTop;
+
+            if (shouldBeFixed && !isNavFixed) {
+                floatingNav.style.position({
+                    position: 'fixed',
+                    top: 0
+                });
+            } else {
+
+            }
+
+        });
+
 
         vm.Answer = _answer;
         vm.Panels = _panels;
